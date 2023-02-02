7256770cffe7b2370e6cd2b5693e6390c28982b5
haonhat0703 <haonhat2729@gmail.com>
2023-02-02 17:48:40 +0700

    add something new

--- message/message.js
+++ message/message.js
@@ -2,6 +2,12 @@ const { EmbedBuilder } = require("discord.js");
 const fetch = (url) =>
   import("node-fetch").then(({ default: fetch }) => fetch(url));
 
+// more function
+const addTop = require("./function/addTop");
+const help = require("./function/help");
+const loadFunc = require("./function/loadFunc");
+const userInfor = require("./function/userInfor");
+
 async function handleMessage(message, client) {
   if (message.author.bot) return;
   // Get the command name and arguments
@@ -57,54 +63,11 @@ async function handleMessage(message, client) {
     } catch (error) {
       console.log(error.message);
     }
-  } else if (msg.startsWith("load", 0)) {
-    msg = msg.replace("load ", "");
-    if (!message.member.voice.channel)
-      return message.channel.send({
-        content: "You need to join a voice channel.",
-        ephemeral: true,
-      });
-    if (!msg)
-      return message.channel.send(
-        "You need to give me a URL or a search term."
-      );
-
-    let res;
-
-    try {
-      // Search for tracks using a query or url, using a query searches youtube automatically and the track requester object
-      res = await client.manager.search(msg, message.author);
-      // Check the load type as this command is not that advanced for basics
-    } catch (err) {
-      console.log(err.message);
-    }
-    let player = client.manager.players.get(message.guild.id);
-    if (!player) {
-      player = client.manager.create({
-        guild: message.guild.id,
-        voiceChannel: message.member.voice.channel.id,
-        textChannel: message.channel.id,
-      });
-      player.connect();
-    }
-
-    for (const track of res.tracks) {
-      await player.queue.add(track);
-    }
-    message.channel.send(
-      `🎶 Đã thêm playlist\n${res.tracks.length} bài từ ${res.playlist.name}.`
-    );
-    if (!player.playing && !player.paused) {
-      player.play();
-      let playembed = new EmbedBuilder()
-        .setDescription(`Playing ${res.tracks[0].title}.`)
-        .setColor(0x222345)
-        .setImage(res.tracks[0].thumbnail);
-      return message.channel.send({
-        embeds: [playembed],
-      });
-    }
-  }
+  } else if (msg.startsWith("load", 0) || msg.startsWith("play", 0))
+    loadFunc(message, client);
+  else if (msg.startsWith("!top", 0)) addTop(message, client);
+  else if (msg.includes("!infor") && msg.includes("<@")) userInfor(message);
+  else if (msg.startsWith("!help")) help(message, client);
 }
 
 module.exports = handleMessage;
