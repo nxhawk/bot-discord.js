7c7f95b7ecf1de37bc95c15cffd2135be8ad2421
haonhat0703 <haonhat2729@gmail.com>
2023-02-02 17:45:08 +0700

    all message type

---
+++ config/data.js
@@ -0,0 +1,23 @@
+//addTop
+//clear
+const data = [
+  { message: "!cat", description: "Get random image a cat" },
+  { message: "!dog", description: "Get random image a dog" },
+  { message: "!dark", description: "Get random meme" },
+  {
+    message: "!quote",
+    description: "Get a sentence of a certain famous person in English",
+  },
+  {
+    message: "load [url]",
+    description: "Add the songs in this [url] to the playlist",
+  },
+  {
+    message: "!top [url]",
+    description: "Add the song in [url] to the top queue",
+  },
+  { message: "!infor @<ID>", description: "Get information of userID<ID>" },
+  { message: "!help", description: "Show help message" },
+];
+
+module.exports = data;
